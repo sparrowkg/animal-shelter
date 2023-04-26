527ff15aeb2d5b35d73f2ae8e2061b2f31017f8e
sparrowkg <vva1981vvavva@gmail.com>
2023-04-26 17:27:38 +0600

    start main and fixed adaptive

--- src/component/nav/index.jsx
+++ src/component/nav/index.jsx
@@ -21,14 +21,20 @@ const BurgerMenu = () => {
     </div>
       
       <div className='burgerMenu'>
-   <button onClick={handleToggleMenu}>menu</button>
+   <button onClick={handleToggleMenu}>
+   <svg className='logoMenu' width="800px" height="800px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M4 17H20M4 12H20M4 7H20" stroke="#ffffff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+   </button>
       {isOpen && (
+      <div>
         <ul>
           <NavLink to="/">Главная</NavLink> 
           <NavLink to="/dogs">Собаки</NavLink> 
           <NavLink to="/cats">Кошки</NavLink>
           <NavLink to="/about">О нас</NavLink> 
         </ul>
+      </div>
       )}
       </div>
    
