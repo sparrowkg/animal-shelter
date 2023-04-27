857c0a1a929d50156722e1007f79aa5f056519d5
suusara <smataeva09@gmail.com>
2023-04-28 00:19:40 +0600

    add about page

--- src/component/Footer/index.jsx
+++ src/component/Footer/index.jsx
@@ -4,11 +4,11 @@ import './Footer.css';
 
 export function Footer() {
   return (
-    <div className="Footer container">
+    <div className="Footer">
         <div className="container1">
         <p className="footer_logo">   © ПРИЮТ БИРЮЛЕВО,  2023</p>
         <div className="footer_icon">
-        <h4 className="friends"> ПОМОГИ СОБАКАМ – РАССКАЖИ О НАС ДРУЗЬЯМ!</h4>
+        <h4 className="footer_friends"> ПОМОГИ СОБАКАМ – РАССКАЖИ О НАС ДРУЗЬЯМ!</h4>
       <div className="footer_icons"> 
        <svg viewBox="0 0 22 35" fill="none" xmlns="http://www.w3.org/2000/svg">
 <path d="M16 1.91833H21V8.33482H17.6667C15.8257 8.33482 14.3333 9.7712 14.3333 11.5431V14.7513H21L19.2608 21.1678H14.0435V34.0008H7.08695V21.1678H1V14.8148H7.66667V9.93894C7.66667 5.50929 11.3977 1.91833 16 1.91833V1.91833Z" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
