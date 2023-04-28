3569f21912a21e4a467616cce47414a423293073
suusara <smataeva09@gmail.com>
2023-04-28 10:27:02 +0600

    add 404 page styles

--- src/pages/not-found/index.jsx
+++ src/pages/not-found/index.jsx
@@ -2,6 +2,9 @@ import './style.css'
 
 export const NotFoundPage = () => {
   return (
-      <div>NotFoundPage</div>
+      <div className='notfoundpage'>
+        <h1 className='notfound'>404</h1>
+        <h3 className='notpage'>Not Found Page</h3>
+      </div>
   )  
 }
