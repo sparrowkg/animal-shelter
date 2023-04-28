0283a2df8f88f34d973045ec0eeac6c59202de7c
suusara <smataeva09@gmail.com>
2023-04-28 12:32:17 +0600

    add pets-item page

--- src/pages/pets-item/index.jsx
+++ src/pages/pets-item/index.jsx
@@ -2,6 +2,25 @@ import './style.css'
 
 export const PetsItemPage = () => {
   return (
-      <div className='container'>PetsItemPage</div>
+    <div className='container'>
+
+      <div className='petsitempage_block'>
+        <img className='petsitempage_img' src="https://images.unsplash.com/photo-1561037404-61cd46aa615b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt="" />
+      <div>
+        <div className='petsitempage_info'>
+         <h3 className='petsitempage_name'>МИСА</h3>
+         <p>Пол питомца:ДЕВОЧКА</p>
+         <p>Озорная и смышлёная крошка</p>
+        </div>
+        
+        <div className='petsitempage_contac'>
+         <h4 className='petsitempage_volonter'>ВОЛОНТЕР:ВАРВАРА</h4>
+         <h4 className='petsitempage_contacts'>ТЕЛЕФОН: +996 555 567 847 </h4>
+        </div>
+      </div>
+       
+      </div>
+       
+     </div>
   )  
 }
