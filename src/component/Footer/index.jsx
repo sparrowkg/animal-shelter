5f53b4e0874b6cc9d8f26c210d2d5e04d9165a6f
suusara <smataeva09@gmail.com>
2023-04-28 11:14:33 +0600

    instagram, twitter

--- src/component/Footer/index.jsx
+++ src/component/Footer/index.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import './Footer.css';
 import { FacebookIcon } from './facebook'
+import { InstagramIcon } from "./instagram";
+import { TwitterIcon } from "./Twitter";
 
 
 export function Footer() {
@@ -12,14 +14,9 @@ export function Footer() {
         <h4 className="footer_friends"> ПОМОГИ СОБАКАМ – РАССКАЖИ О НАС ДРУЗЬЯМ!</h4>
       <div className="footer_icons"> 
         <FacebookIcon />
-<svg  viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M1 7.6181C1 4.42874 3.68629 1.84326 7 1.84326H25C28.3137 1.84326 31 4.42874 31 7.6181V24.9426C31 28.1319 28.3137 30.7174 25 30.7174H7C3.68629 30.7174 1 28.1319 1 24.9426V7.6181Z"  stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-<path d="M16.0002 22.697C19.6821 22.697 22.6668 19.8242 22.6668 16.2805C22.6668 12.7368 19.6821 9.86401 16.0002 9.86401C12.3183 9.86401 9.3335 12.7368 9.3335 16.2805C9.3335 19.8242 12.3183 22.697 16.0002 22.697Z"  stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-<path d="M25 7.64937V7.61804" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-</svg>
-<svg  viewBox="0 0 32 28" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M10.432 26.3112C21.748 26.3112 27.94 16.8613 27.94 8.68026C27.94 8.4144 27.94 8.14856 27.928 7.8827C29.128 7.00055 30.172 5.91296 31 4.68036C29.872 5.17582 28.684 5.51418 27.46 5.65918C28.744 4.8858 29.704 3.66528 30.16 2.22726C28.948 2.95232 27.628 3.45985 26.248 3.72571C23.92 1.22426 20.02 1.10342 17.536 3.45985C15.94 4.97039 15.256 7.23014 15.76 9.38115C10.828 9.12738 6.208 6.77094 3.088 2.90397C1.456 5.7317 2.284 9.35698 4.996 11.1696C4.012 11.1455 3.064 10.8796 2.2 10.3962V10.4687C2.2 13.4173 4.264 15.9549 7.144 16.5471C6.232 16.8009 5.284 16.8372 4.36 16.6559C5.164 19.1815 7.48 20.9096 10.108 20.958C7.924 22.6859 5.236 23.6164 2.464 23.6164C1.972 23.6164 1.48 23.5802 1 23.5318C3.82 25.3565 7.084 26.3112 10.432 26.3112V26.3112Z" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-</svg></div>
+        <InstagramIcon />
+        <TwitterIcon />
+      </div>
         </div>
 <div className="footer_email">
 <svg  viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg">
