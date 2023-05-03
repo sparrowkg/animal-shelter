3eb9b32f1b421f41b9326f8e420049fa11aff872
suusara <smataeva09@gmail.com>
2023-05-03 11:00:24 +0600

    style aboutPage

--- src/pages/about/index.jsx
+++ src/pages/about/index.jsx
@@ -3,7 +3,7 @@ export const AboutPage = () => {
     return (
         <div className="about_title">
          <div className="about_container"> 
-            <h3>О НАС</h3>
+            <h3 className="about_title_text" >О НАС</h3>
             <p className="about_t">МЫ - ВОЛОНТЕРЫ  МУНИЦИПАЛЬНОГО ПРИЮТА ДЛЯ БЕЗДОМНЫХ ЖИВОТНЫХ </p>
            
             <img className="about_img" src="https://images.unsplash.com/photo-1507682520764-93440a60e9b5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=725&q=80" alt="" />
