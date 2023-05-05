6a1c3e812fdaf7ae12261daeb335ba6b6213b33d
suusara <smataeva09@gmail.com>
2023-05-05 19:22:22 +0600

    add style button close

--- src/pages/pets-item/index.jsx
+++ src/pages/pets-item/index.jsx
@@ -20,14 +20,17 @@ import { useParams,useNavigate } from 'react-router-dom'
    return (
    
      <div className='container'>
-
-     
+    
      <div className='petsitempage_block'>
+     
         {
          <img className='petsitempage_img' src={li.main_image} alt="" />
         }
       <div>
-      <button className='button-pet-item' onClick={()=>history(-1)}>Назад</button>
+      <button className='button-pet-item' onClick={()=>history(-1)}><svg className='close_svg' width="28" height="28" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M18 6L6 18" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M6 6L18 18" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+</svg></button>
         <div className='petsitempage_info'>
           
           <div className='petsitem_name_male'>
